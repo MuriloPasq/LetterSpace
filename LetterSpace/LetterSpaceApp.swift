ad20d2239de9897191933ad5254fc868045ddd07
MuriloPasq <murilopasquim@gmail.com>
2023-09-27 09:09:28 -0300

    Initial Commit

---
+++ LetterSpace/LetterSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterSpaceApp.swift
+//  LetterSpace
+//
+//  Created by Murilo Correa Pasquim on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LetterSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
